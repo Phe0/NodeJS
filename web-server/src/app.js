cb911bfb3cc701097e4f0af3ea8849d95e377f78
phe0 <pheofo@gmail.com>
2019-07-19 01:20:58 -0300

    End of section 7

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,66 @@
+const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
-app.get('', (req, res) => {
-    res.send('Hello express!')
-})
 
-app.get('/help', (req, res) => {
-    res.send('Help page')
+//Define paths for Express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve 
+app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Robot'
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.render('about', {
+        title: 'About',
+        name: 'Robot'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Robot',
+        message: 'Help message'
+    })
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Your weather')
+    res.send({
+        forecast: 'forecast',
+        location: 'location'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Robot',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Robot',
+        errorMessage: 'Page not found'
+    })
 })
 
 app.listen(3000, () => {
